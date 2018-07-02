11a26a78bc7d2a8c9b66ec2fe05afa7a1490ce10
Gerald Versluis <gerald@verslu.is>
2018-07-02 19:44:29 +0200

    Initial commit

---
+++ CrossSimpleAudioPlayerSample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrossSimpleAudioPlayerSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		public void Handle_Clicked(object sender, EventArgs e)
+		{
+			Play();
+		}
+
+		private void Play()
+		{
+			var assembly = typeof(App).GetTypeInfo().Assembly;
+			Stream audioStream = assembly.GetManifestResourceStream("CrossSimpleAudioPlayerSample.Resources.sheep.wav");
+			var audio = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+			audio.Load(audioStream);
+
+			audio.Play();
+		}
+	}
+}
